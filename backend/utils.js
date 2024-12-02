6fc70b788721510159fad1187a0c2a1cb07f37e4
unknown <miguel.recinos.tab@gmail.com>
2024-12-02 08:08:04 -0600

    Iniciando proyecto

---
+++ backend/utils.js
@@ -0,0 +1,14 @@
+module.exports = {
+    makeid,
+}
+
+function makeid(lenght){
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLenght = characters.length;
+    for(var i = 0; i < lenght; i++){
+        result += characters.charAt(Math.floor(Math.random() * charactersLenght));
+    }
+    return result;
+}
+
